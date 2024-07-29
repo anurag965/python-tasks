b17f6bf50683466eb5220bc37955b19de57c4291
anurag965 <134576365+anurag965@users.noreply.github.com>
2024-07-29 23:14:22 +0530

    task 2.py

---
+++ task 2.py
@@ -0,0 +1,11 @@
+x=float(input("enter the first number: "))
+y=float(input("enter the second number: "))
+z=input("enter the operator symbol: ")
+if z=="+":
+    print(x+y)
+elif z=="-":
+    print(x-y)
+elif z=="*":
+    print(x*y)
+elif z=="/":
+    print(x/y)
